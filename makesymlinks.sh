5758b926e84c68a6f70f114ddc3731a0a8485d0f
cameljon <volden.jon@gmail.com>
2018-02-22 15:14:07 -0600

    Added tmux. Cleaned up makesymlinks.sh

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,51 +1,59 @@
 #!/bin/bash
-############################
+
 # .make.sh
 # This script creates symlinks from the home directory to any desired dotfiles 
 # in ~/dotfiles
-############################
 
-########## Variables
-
-dir=~/dotfiles     # dotfiles directory
+# Variables
+dir=~/dotfiles      # dotfiles directory
 olddir=~/.old       # old dotfiles backup directory
 os=`uname -o`
 
-# list of files/folders to symlink in homedir
-files="minttyrc profile bashrc vimrc gitconfig zshrc dir_colors vim"
+# Colors
+red=$'\e[1;31m'
+end=$'\e[0m'
 
-########## Move files and link new ones.
+# List of files. Space seperated.
+files="tmux.conf minttyrc profile bashrc vimrc gitconfig zshrc dir_colors vim"
 
-# Create .old in homedir
-echo "Running symlinks on $os"
-echo "Creating $olddir for backup of any existing dotfiles in ~"
+# Pre-install
+printf "\nRunning symlinks script\n"
+printf "Detected OS: ${red}%s${end}\n" "[$os]"
+printf "Dotfiles: ${red}%s${end}\n" "$files"
+printf "Creating backup folder: ${red}%s${end}\n" "[$olddir]"
+if [ -a $olddir ]; then
+    printf "Backup directory already exists.\n"
+    exit
+fi
 mkdir -p $olddir
-echo "...done"
 
-# Change to the dotfiles directory
-echo "Changing to the $dir directory"
+# Change working directory
+printf "Changing directory: ${red}%s${end}\n" "[$dir]"
 cd $dir
-echo "...done"
 
-# Move any existing dotfiles in homedir to .old directory, then create symlinks 
+# Backup files
+printf "\n"
 for file in $files; do
-    if [ -a $file ]; then
-            echo "Moving $file from ~/ to $olddir"
-            mv ~/.$file $olddir
-        else
-            echo "$file does not exist. Not moving anything."
-    fi
-    # Only create symlink for mintty if we are in Cywin.
-    if [ $os == "Cygwin" -a $file == "mintyrc" ]; then
-        echo "Creating symlink to $file in home directory."
-        ln $dir/$file ~/.$file
-    # Everything else.
+    if [ -a ~/.$file ]; then
+        printf "Backing up: ${red}%s${end}\n" "[.$file]"
+        mv ~/.$file $olddir
     else
-        echo "Creating symlink to $file in home directory."
+        echo ".$file does not exist. Not moving anything."
+    fi
+done
+
+# Create symlinks
+printf "\n"
+for file in $files; do
+    if [ $os == "Cygwin" -a $file == "minttyrc" ]; then
+        printf "Creating symlink: ${red}%s${end}[cygwin only]\n" "[$file]"
+        ln $dir/$file ~/.$file
+    else
+        printf "Creating symlink: ${red}%s${end}\n" "[$file]"
         ln -s $dir/$file ~/.$file
     fi
 done
 
-echo "After new shell installs, run the following commands."
-echo "> chsh -s \$(which zsh)"
-echo "> cd ~/dotfiles && git submodule init && git submodule update"
+# Post install
+printf "\nAfter new shell installs, run the following command.\n"
+printf "> ${red}cd ~/dotfiles && git submodule init && git submodule update${end}\n\n"
